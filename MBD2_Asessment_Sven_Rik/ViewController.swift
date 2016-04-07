4ecc4d3186fa1f6c410ce3c916804c087ceb1c8e
Uitleen <uitleen@009029.local>
2016-04-07 20:57:19 +0200

    Initial Commit

---
+++ MBD2_Asessment_Sven_Rik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MBD2_Asessment_Sven_Rik
+//
+//  Created by Uitleen on 07/04/16.
+//  Copyright © 2016 Sven Brettschneider & Rik van den Heuvel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
